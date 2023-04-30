a64a71faa67afa9e303fd579ccff544d6f619ef6
HeartFuentes <93439269+HeartFuentes@users.noreply.github.com>
2023-04-30 19:49:18 +0800

    Add files via upload

---
+++ fireconfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBLZls2wCdcdznBWMCgz_0uyuEI24GF8C0",
+    authDomain: "heartfire-35bd8.firebaseapp.com",
+    databaseURL: "https://heartfire-35bd8-default-rtdb.firebaseio.com",
+    projectId: "heartfire-35bd8",
+    storageBucket: "heartfire-35bd8.appspot.com",
+    messagingSenderId: "800764103191",
+    appId: "1:800764103191:web:7e6f606a737133c500ea52"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
